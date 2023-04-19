968ee9ef221f93e6cffed14979078da3778ca8bf
seaung <seaung.c0m7de@protonmail.com>
2023-04-19 23:46:34 +0800

    完成基本的封装，目前账号没申请成功，没法测试

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # anthropic-go
+一个封装了anthropic API的golang库(目前没有申请到claude账号,所以此库还没验证过)
+
+
+---
+that's all
--- anthropic/anthropic.go
+++ anthropic/anthropic.go
@@ -1,12 +1,18 @@
 package anthropic
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"net/url"
 	"os"
+	"strings"
 	"sync"
 	"time"
 )
@@ -17,6 +23,10 @@ const (
 	apiPrompt   = "\n\nAssistant:"
 	HumanPrompt = "\n\nHuman:"
 )
+
+var ReadBodyError = errors.New("could not read error response.")
+
+type ErrorHandler func(*http.Response) error
 
 type Client struct {
 	APIKey   string
@@ -81,8 +91,7 @@ func (c *Client) SetTimeout(timeout int) {
 	c.Client.Timeout = time.Duration(timeout)
 }
 
-func (c *Client) newRequest(body io.Reader) (*http.Request, error) {
-	uri := fmt.Sprintf("%s/v1/complete", baseURL)
+func (c *Client) newRequest(uri string, body io.Reader) (*http.Request, error) {
 	req, err := http.NewRequest(http.MethodPost, uri, body)
 	if err != nil {
 		return nil, err
@@ -101,14 +110,88 @@ func (c *Client) do(cxt context.Context, request *http.Request) (*http.Response,
 	}
 
 	if c.Debug {
+		c.dumpRequest(request)
 	}
 
 	return c.Client.Do(request)
 }
 
-func (c *Client) dumpRequest(request *http.Request) {}
+func (c *Client) dumpRequest(request *http.Request) {
+	var body string
+	if request.Body != nil {
+		byteContent, err := ioutil.ReadAll(request.Body)
+		if err != nil {
+			log.Printf("[DEBUG] failed to read body : %s\n", err)
+		}
 
-func (c *Client) Completion(cxt context.Context) (*CompletionResponse, error) {
+		request.Body = ioutil.NopCloser(bytes.NewReader(byteContent))
+		body = string(byteContent)
+	}
+
+	message := fmt.Sprintf("%s %s %s", request.Method, request.URL.String(), body)
+	log.Printf("[DEBUG] client request : %s\n", message)
+}
+
+func (c *Client) parseResponseContent(destination interface{}, body io.Reader) error {
+	var err error
+	if word, ok := destination.(io.Writer); ok {
+		_, err = io.Copy(word, body)
+	} else {
+		decoder := json.NewDecoder(body)
+		err = decoder.Decode(destination)
+	}
+
+	return err
+}
+
+func (c *Client) DoWithErrorHanding(ctx context.Context, request *http.Request, destination interface{}, errHandler ErrorHandler) error {
+	response, err := c.do(ctx, request)
+	if err != nil {
+		return err
+	}
+
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		return errHandler(response)
+	}
+
+	if destination == nil {
+		return nil
+	}
+
+	return c.parseResponseContent(destination, response.Body)
+}
+
+func (c *Client) Do(ctx context.Context, request *http.Request, destination interface{}) error {
+	return c.DoWithErrorHanding(ctx, request, destination, getErrorFromResponse)
+}
+
+func getErrorFromResponse(r *http.Response) error {
+	rs := new(struct {
+		Error string `json:"error"`
+	})
+	msg, err := ioutil.ReadAll(r.Body)
+	if err == nil {
+		if err := json.Unmarshal(msg, rs); err == nil {
+			return errors.New(rs.Error)
+		}
+		return errors.New(strings.TrimSpace(string(msg)))
+	}
+	return ReadBodyError
+}
+
+func (c *Client) Completion(ctx context.Context, parameters url.Values) (*CompletionResponse, error) {
 	var completion CompletionResponse
+	uri := fmt.Sprintf("%s/v1/complete", baseURL)
+
+	req, err := c.newRequest(uri, strings.NewReader(parameters.Encode()))
+	if err != nil {
+		return nil, err
+	}
+
+	if err := c.Do(ctx, req, &completion); err != nil {
+		return nil, err
+	}
 	return &completion, nil
 }
